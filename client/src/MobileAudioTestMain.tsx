2ba39131bbecc05dd384a933e2d1e892618372ab
David Volta <davidalanvolta@gmail.com>
2025-07-22 11:19:30 -0700

    CLEAN STABLE VERSION - remove mobile test files

--- client/src/MobileAudioTestMain.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import MobileAudioTest from './MobileAudioTest.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <MobileAudioTest />
-)
